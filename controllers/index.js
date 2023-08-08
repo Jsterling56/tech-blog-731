8cd26ad5e3b822bba3fadf3e938532c12db6d53e
JSterling56 <jtsterlin@yahoo.com>
2023-08-08 17:35:09 -0400

    debugged some stuff

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const apiRoutes = require('./api'); // Import the apiRoutes module
+const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
 const homeRoutes = require('./home-routes.js');
 const { Post, Comment, User } = require(path.join(__dirname, '..', 'models'));
@@ -11,7 +11,7 @@ const { Post, Comment, User } = require(path.join(__dirname, '..', 'models'));
 // Define your routes here
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('home-routes', homeRoutes);
+router.use('/', homeRoutes); // Change the route to use the root path '/'
 // Add more routes as needed
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 const express = require('express');
-const path = require('path');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const path = require('path');
+const helpers = require('./utils/helpers')
+const session = require('express-session');
+const routes = require('./controllers'); // Assuming your routes are defined in a file like controllers/index.js
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({ helpers });
-
 const sess = {
-  cookie: {},
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
+  //   Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
+
+app.use(session(sess));
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRoutes);
-app.use('/dashboard', dashboardRoutes);
+app.use(routes);
+sequelize.sync({force:false}).then (function(){
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+})
 
-sequelize.sync();
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
